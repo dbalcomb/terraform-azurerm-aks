a1e151bff0e6f9bbdd271c78131f3253480b9f8e
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-27 16:42:15 +0000

    Add live data monitoring integration (#15)

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    azurerm = ">= 1.41"
+    azurerm    = ">= 1.41"
+    kubernetes = ">= 1.10"
   }
 }
 
@@ -105,4 +106,34 @@ resource "azurerm_role_assignment" "monitor" {
   principal_id         = var.service_principal.id
   scope                = azurerm_kubernetes_cluster.main.id
   role_definition_name = "Monitoring Metrics Publisher"
+}
+
+resource "kubernetes_cluster_role" "monitor" {
+  metadata {
+    name = "aks-monitor"
+  }
+
+  rule {
+    api_groups = ["", "metrics.k8s.io", "extensions", "apps"]
+    resources  = ["pods", "pods/log", "events", "nodes", "deployments", "replicasets"]
+    verbs      = ["get", "list", "top"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "monitor" {
+  metadata {
+    name = "aks-monitor"
+  }
+
+  role_ref {
+    name      = "aks-monitor"
+    kind      = "ClusterRole"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    name      = "clusterUser"
+    kind      = "User"
+    api_group = "rbac.authorization.k8s.io"
+  }
 }
