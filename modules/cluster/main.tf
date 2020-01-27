052da32efb241d6def760d2c41c2a4ab4d06d3a0
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-27 12:08:15 +0000

    Add support for defining multiple node pools

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,3 +1,10 @@
+locals {
+  pools = {
+    for name, pool in var.pools : name => pool
+    if name != "primary"
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = format("%s-rg", var.name)
   location = var.location
@@ -11,10 +18,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    name           = "node"
-    node_count     = 1
-    vm_size        = "Standard_D2_v2"
-    vnet_subnet_id = var.network.subnets.primary.id
+    name            = "primary"
+    type            = "VirtualMachineScaleSets"
+    vnet_subnet_id  = var.network.subnets.primary.id
+    vm_size         = var.pools.primary.size
+    node_count      = var.pools.primary.scale
+    max_pods        = var.pools.primary.pod_limit
+    os_disk_size_gb = var.pools.primary.disk_size
   }
 
   service_principal {
@@ -25,6 +35,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin     = "azure"
     network_policy     = "azure"
+    load_balancer_sku  = "standard"
     dns_service_ip     = var.network.dns_service_ip
     service_cidr       = var.network.service_cidr
     docker_bridge_cidr = var.network.docker_bridge_cidr
@@ -36,6 +47,18 @@ resource "azurerm_kubernetes_cluster" "aks" {
       log_analytics_workspace_id = var.monitor.log_analytics_workspace.id
     }
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "additional" {
+  for_each              = local.pools
+  name                  = each.key
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vnet_subnet_id        = var.network.subnets[each.value.subnet].id
+  vm_size               = each.value.size
+  node_count            = each.value.scale
+  max_pods              = each.value.pod_limit
+  os_disk_size_gb       = each.value.disk_size
+  os_type               = "Linux"
 }
 
 resource "azurerm_role_assignment" "network" {
