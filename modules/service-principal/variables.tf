ab9b5edd29e14804a56a82b0acb41c42e9ae63cc
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-30 15:54:54 +0000

    Move role assignments to service principal module

--- modules/service-principal/variables.tf
+++ modules/service-principal/variables.tf
@@ -2,3 +2,12 @@ variable "name" {
   description = "The Service Principal App Registration name"
   type        = string
 }
+
+variable "role_assignments" {
+  description = "The role assignments"
+  default     = {}
+  type = map(object({
+    scope = string
+    role  = string
+  }))
+}
