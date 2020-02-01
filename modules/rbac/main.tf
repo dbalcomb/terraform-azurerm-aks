1f72e04c18a75b0fc7c07028bd7f06d0a32d0a03
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-02-01 14:39:19 +0000

    Move nested modules to parent directory

--- modules/rbac/main.tf
+++ modules/rbac/main.tf
@@ -1,12 +1,12 @@
 module "server" {
-  source  = "./apps/server"
+  source  = "./server"
   name    = format("%s-server", var.name)
   consent = var.consent
   enabled = var.enabled
 }
 
 module "client" {
-  source  = "./apps/client"
+  source  = "./client"
   name    = format("%s-client", var.name)
   server  = module.server
   enabled = var.enabled
