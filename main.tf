a2a6a7cc55086131c3c24ad5aa2da1413f004d81
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-02-11 21:26:47 +0000

    Add kubernetes and helm provider configuration (#42)

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+provider "kubernetes" {
+  host                   = module.cluster.kubernetes.host
+  username               = module.cluster.kubernetes.username
+  password               = module.cluster.kubernetes.password
+  client_certificate     = base64decode(module.cluster.kubernetes.client_certificate)
+  client_key             = base64decode(module.cluster.kubernetes.client_key)
+  cluster_ca_certificate = base64decode(module.cluster.kubernetes.cluster_ca_certificate)
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.cluster.kubernetes.host
+    username               = module.cluster.kubernetes.username
+    password               = module.cluster.kubernetes.password
+    client_certificate     = base64decode(module.cluster.kubernetes.client_certificate)
+    client_key             = base64decode(module.cluster.kubernetes.client_key)
+    cluster_ca_certificate = base64decode(module.cluster.kubernetes.cluster_ca_certificate)
+    load_config_file       = false
+  }
+}
+
 module "service_principal" {
   source = "./modules/service-principal"
   name   = var.name
